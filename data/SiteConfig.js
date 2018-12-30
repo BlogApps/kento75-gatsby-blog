fba44eb651eaa0c63fd7cdcc5f508786c6e88021
Kento75 <kento2github@gmail.com>
2018-12-30 22:05:51 +0900

    Fix config.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   siteTitleAlt: "Kento74 Blog", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://kento75blog.netlify.com", // Domain of your website without pathPrefix.
-  pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
