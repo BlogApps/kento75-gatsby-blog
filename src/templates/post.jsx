22fc87bb6d2d6c2313c875ca7510642691776753
Kento75 <kento2github@gmail.com>
2019-02-07 06:56:48 +0900

    Fix disbled disqus comment

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -92,7 +92,7 @@ export default class PostTemplate extends React.Component {
               config={config}
               expanded={expanded}
             />
-            <Disqus postNode={postNode} expanded={expanded} />
+            {/* <Disqus postNode={postNode} expanded={expanded} /> */}
           </div>
 
           <PostSuggestions postNode={postNode} />
