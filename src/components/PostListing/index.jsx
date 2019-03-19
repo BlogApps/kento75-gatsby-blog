f148bb587de079f46f09990b92832d325372537e
Kento75 <kento2github@gmail.com>
2019-03-19 13:06:03 +0900

    Del SideBar components

--- src/components/PostListing/index.jsx
+++ src/components/PostListing/index.jsx
@@ -1,43 +1,36 @@
-import React from "react";
-import PostPreview from "../PostPreview";
-//import Sidebar from "../Sidebar";
-import TagList from "../TagList";
-import "./PostListing.scss";
+import React from 'react';
+import PostPreview from '../PostPreview';
+import TagList from '../TagList';
+import './PostListing.scss';
 
 class PostListing extends React.Component {
-  getPostList() {
+  getPostList () {
     const postList = [];
-    this.props.postEdges.forEach(postEdge => {
-      postList.push({
+    this.props.postEdges.forEach (postEdge => {
+      postList.push ({
         path: postEdge.node.fields.slug,
         tags: postEdge.node.frontmatter.tags,
         cover: postEdge.node.frontmatter.cover,
         title: postEdge.node.frontmatter.title,
         date: postEdge.node.fields.date,
         excerpt: postEdge.node.excerpt,
-        timeToRead: postEdge.node.timeToRead
+        timeToRead: postEdge.node.timeToRead,
       });
     });
     return postList;
   }
-  render() {
-    const postList = this.getPostList();
+  render () {
+    const postList = this.getPostList ();
     return (
       <div className="md-grid md-grid--no-spacing md-cell--middle">
         <div className="md-grid md-cell--8 mobile-fix">
-          {postList.map(post => (
+          {postList.map (post => (
             <PostPreview key={post.title} postInfo={post} />
           ))}
           <span className="tagList-area">
             <TagList postList={postList} />
           </span>
         </div>
-        {/* <div className="md-grid md-cell--4 mobile-fix">
-          <div className="tagList-area">
-            <Sidebar postList={postList}/>
-            <br/>
-          </div>
-        </div> */}
       </div>
     );
   }
